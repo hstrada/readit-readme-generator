35aa580a7acfc1d6ada2e550da602fe8a8ca1bb0
hstrada <h.strada@hotmail.com>
2020-08-30 17:53:32 -0300

    add dependencies

---
+++ rhex/src/components/CustomInput/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container, Input } from "./styles";
+
+const CustomInput = () => {
+  <Container>
+    <Input />
+  </Container>;
+};
+
+export default CustomInput;
