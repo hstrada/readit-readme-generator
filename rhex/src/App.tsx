29493f8f1c65ffa265bb911c4113da69a13470ba
Helena Strada <h.strada@hotmail.com>
2020-09-01 12:51:18 -0300

    feat: upload project

--- rhex/src/App.tsx
+++ rhex/src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-
-import { CustomInput } from './components';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      hello world
-      <CustomInput value="hello" onChange={(e) => console.log(e)} />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- rhex/src/components/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as CustomInput } from './CustomInput';
