b188697c7c1780daf97e9af8d2eb55871da1fe96
hstrada <h.strada@hotmail.com>
2020-09-01 20:00:24 -0300

    feat: create input from list

--- rhex/src/App.tsx
+++ rhex/src/App.tsx
@@ -1,7 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function App() {
-  return <div>hello world</div>;
-}
+const TextInput = ({
+  value,
+  onChange,
+  key,
+}: {
+  value: string;
+  key: string | number;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}) => (
+  <div>
+    <input type="text" key={key} value={value} onChange={onChange} />
+  </div>
+);
+
+const App = () => {
+  const questions = [
+    {
+      question: 'Pergunta A',
+      answer: '',
+    },
+    {
+      question: 'Pergunta B',
+      answer: '',
+    },
+  ];
+
+  const [values, setValues] = useState(questions);
+
+  const handleQA = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
+    const insertQA = [...values];
+
+    const selectedQuestion = insertQA[index];
+    selectedQuestion.answer = e.target.value;
+
+    setValues(insertQA);
+  };
+
+  return (
+    <div>
+      <br />
+      {values.map((value, index) => {
+        console.log(value);
+        return (
+          <label>
+            {value.question}
+            <TextInput
+              key={index}
+              value={value.answer}
+              onChange={e => handleQA(e, index)}
+            />
+          </label>
+        );
+      })}
+    </div>
+  );
+};
 
 export default App;
