d3d202f8fc03cc10a992b34e036af2afe502ea4a
Paulo Roberto Brandão da silva <paulo.brandao@info.local>
2020-10-19 15:27:57 -0300

    fix: Button fixed

--- readit/src/components/Button/Default/styles.tsx
+++ readit/src/components/Button/Default/styles.tsx
@@ -9,6 +9,7 @@ export const DefaultContainer = styled.button`
   width: 100%;
   background-color: ${({ theme }) => theme.colors.button.flat.background};
   color: ${({ theme }) => theme.colors.button.flat.font};
+  transition: all .4s;
 
   @media (min-width: 720px) {
     width: initial;
--- readit/src/components/Button/Flat/styles.tsx
+++ readit/src/components/Button/Flat/styles.tsx
@@ -5,6 +5,7 @@ import { DefaultContainer } from '../Default/styles';
 import { StyledProps } from './interface';
 
 export const Container = styled(DefaultContainer)<StyledProps>`
+  
   &:hover {
     background-color: #085ddd;
   }
